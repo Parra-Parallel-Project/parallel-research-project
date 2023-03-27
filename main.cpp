d622f3984479e20f4ff9af9f24a57305f50b80d5
Ethan <bulldawg0065@gmail.com>
2023-03-27 00:30:35 -0400

    Wrote structure for testing functions in runner.

--- main.cpp
+++ main.cpp
@@ -14,15 +14,28 @@ using namespace std;
 // Used to test algorithms: (start, end, graph)
 typedef tuple<int, int, WeightedGraph> problem;
 
+struct arr2d {
+    int a[n_groups][n_graphs];
+};
+
+struct arr3d {
+    int a[n_thread_groups][n_groups][n_graphs];
+};
+
 
 // Helper functions
-problem generateNewProblem(int n);
+problem generateNewProblem(int n); // generates a new graph with guaranteed path between start and stop nodes
 void printEdgesPerProblem(vector<vector<problem>> arr); // debug print
+arr2d testUnthreaded(int alg); // performs test for one unthreaded  algorithm and measures the times
+arr3d testThreaded(int alg); // performs test for one threaded algorithm and measures the times across different threadcounts
 
-
+// globals
 static const int n_graphs = 100; // number of test problems per group
 static const int n_groups = 5; // number of groups to test
 static const int groups[] = {10, 50, 100, 1000, 10000}; // nodes used in each group
+static const int n_thread_groups = 4; // number of different thread counts to test
+static const int thread_groups[] = {1, 2, 4, 8}; // threads to test each multithreaded solution using
+
 
 int main() {
 
@@ -33,6 +46,7 @@ int main() {
 
     vector<vector<problem>> allProblems;
 
+    // generate all problems to be solved
     for(int group = 0; group < n_groups; group++) {
 
         vector<problem> currentGroup;
@@ -46,10 +60,16 @@ int main() {
         allProblems.push_back(currentGroup);        
     }
 
-    printEdgesPerProblem(allProblems);
+    // printEdgesPerProblem(allProblems); // DEBUG
+
+    for(int alg = 0; alg < 4; alg++) { // do each algorithm type independently
+
+        arr2d times = testUnthreaded(alg);
+    }
+
 
 }
- 
+
 
 problem generateNewProblem(int n) {
     
@@ -97,6 +117,21 @@ problem generateNewProblem(int n) {
 
     // cout << "Edges Attempted: " << nEdges << "\t\tActual: " << g.getnEdges() << "\n\n"; // DEBUG
     return problem(start, stop, g);
+}
+
+
+arr2d testUnthreaded(int alg) {
+
+    arr2d times; // times for unthreaded solutions
+
+    return times;
+}
+
+arr3d testThreaded(int alg) {
+
+    arr3d times; // times for threaded solutions
+
+    return times;
 }
 
 
