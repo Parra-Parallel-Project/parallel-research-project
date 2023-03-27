880563dca1a02146f3b1e1be917516d04357b634
Ethan <bulldawg0065@gmail.com>
2023-03-27 14:58:38 -0400

    Added debugging output, set up testing functions

--- main.cpp
+++ main.cpp
@@ -4,17 +4,38 @@
 #include <tuple>
 #include <Vector>
 #include <iomanip>
+#include <string>
+#include <chrono>
 
 #include "WeightedGraph.h"
 
 using namespace std;
+using namespace std::chrono;
 
 #define MAX_WEIGHT 10
 
 // Used to test algorithms: (start, end, graph)
 typedef tuple<int, int, WeightedGraph> problem;
 
-struct arr2d {
+
+// globals
+
+// per unthreaded alg 
+static const int n_graphs = 10; // number of test problems per group
+static const int n_groups = 5; // number of groups to test
+static const int groups[] = {10, 50, 100, 1000, 10000}; // nodes used in each group
+
+// per multithreaded alg
+static const int n_thread_groups = 4; // number of different thread counts to test
+static const int thread_groups[] = {1, 2, 4, 8}; // threads to test each multithreaded solution using
+
+// the algs
+static const int nAlgs = 4;
+static const string algNames[nAlgs] = {"Depth_First_Search", "Breadth_First_Search", "Dijkstra", "A_Star"};
+
+
+// for returning arrays from a fnc
+struct arr2d { 
     int a[n_groups][n_graphs];
 };
 
@@ -26,15 +47,10 @@ struct arr3d {
 // Helper functions
 problem generateNewProblem(int n); // generates a new graph with guaranteed path between start and stop nodes
 void printEdgesPerProblem(vector<vector<problem>> arr); // debug print
-arr2d testUnthreaded(int alg); // performs test for one unthreaded  algorithm and measures the times
-arr3d testThreaded(int alg); // performs test for one threaded algorithm and measures the times across different threadcounts
-
-// globals
-static const int n_graphs = 100; // number of test problems per group
-static const int n_groups = 5; // number of groups to test
-static const int groups[] = {10, 50, 100, 1000, 10000}; // nodes used in each group
-static const int n_thread_groups = 4; // number of different thread counts to test
-static const int thread_groups[] = {1, 2, 4, 8}; // threads to test each multithreaded solution using
+arr2d testUnthreaded(int alg, vector<vector<problem>> graphs); // performs test for one unthreaded  algorithm and measures the times
+arr2d testThreaded(int alg, int threadCount, vector<vector<problem>> graphs); // performs test for one threaded algorithm and measures the times across different threadcounts
+void unthreadedToCSV(int alg);
+void prettyPrint2d(arr2d arr, int height, int width);
 
 
 int main() {
@@ -61,10 +77,18 @@ int main() {
     }
 
     // printEdgesPerProblem(allProblems); // DEBUG
+    arr2d times;
 
     for(int alg = 0; alg < 4; alg++) { // do each algorithm type independently
 
-        arr2d times = testUnthreaded(alg);
+        times = testUnthreaded(alg, allProblems); // unthreaded 
+        prettyPrint2d(times, n_groups, n_graphs);
+
+        for(int t = 0; t < n_thread_groups; t++) { // threaded
+
+            times = testThreaded(alg, thread_groups[t], allProblems);
+            prettyPrint2d(times, n_groups, n_graphs);
+        }
     }
 
 
@@ -120,16 +144,109 @@ problem generateNewProblem(int n) {
 }
 
 
-arr2d testUnthreaded(int alg) {
+arr2d testUnthreaded(int alg, vector<vector<problem>> graphs) {
 
+    cout << "\nRunning tests for unthreaded " << algNames[alg] << "\n";
+    
     arr2d times; // times for unthreaded solutions
+
+    int start, stop;
+
+    for(int i = 0; i < n_groups; i++) {
+
+        for(int j = 0; j < n_graphs; j++) {
+
+            start = get<0>(graphs[i][j]);
+            stop = get<1>(graphs[i][j]);
+            WeightedGraph graph = get<2>(graphs[i][j]);
+            
+
+            auto start = high_resolution_clock::now(); // start time
+            
+            switch(alg) {
+
+                case 0: // DFS
+                    
+                    // unthreadedDFS(start, stop, graph); 
+                    break;
+
+                case 1: // BFS
+
+                    // unthreadedBFS(start, stop, graph); 
+                    break;
+
+                case 2: // Dijkstra's
+
+                    // unthreadedDijkstra(start, stop, graph); 
+                    break;
+
+                case 3: // A*
+
+                    // unthreadedAStar(start, stop, graph); 
+                    break;
+                default:
+                    cout << "What are you doing here?\n";
+            }
+
+            auto stop = high_resolution_clock::now(); // end time
+
+            times.a[i][j] = duration_cast<milliseconds>(stop - start).count();
+        }
+    }
 
     return times;
 }
 
-arr3d testThreaded(int alg) {
 
-    arr3d times; // times for threaded solutions
+arr2d testThreaded(int alg, int threadCount, vector<vector<problem>> graphs) {
+
+    cout << "\nRunning tests for threaded " << algNames[alg] << " using " << threadCount << " threads\n";
+    
+    arr2d times; // times for threaded solutions
+
+    int start, stop;
+
+    for(int i = 0; i < n_groups; i++) {
+
+        for(int j = 0; j < n_graphs; j++) {
+
+            start = get<0>(graphs[i][j]);
+            stop = get<1>(graphs[i][j]);
+            WeightedGraph graph = get<2>(graphs[i][j]);
+            
+
+            auto start = high_resolution_clock::now(); // start time
+            
+            switch(alg) {
+
+                case 0: // DFS
+                    
+                    // threadedDFS(start, stop, graph, threadCount); 
+                    break;
+
+                case 1: // BFS
+
+                    // threadedBFS(start, stop, graph, threadCount); 
+                    break;
+
+                case 2: // Dijkstra's
+
+                    // threadedDijkstra(start, stop, graph, threadCount); 
+                    break;
+
+                case 3: // A*
+
+                    // threadedAStar(start, stop, graph, threadCount); 
+                    break;
+                default:
+                    cout << "What are you doing here?\n";
+            }
+
+            auto stop = high_resolution_clock::now(); // end time
+
+            times.a[i][j] = duration_cast<milliseconds>(stop - start).count();
+        }
+    }
 
     return times;
 }
@@ -147,3 +264,17 @@ void printEdgesPerProblem(vector<vector<problem>> arr) {
     }
 }
 
+
+void prettyPrint2d(arr2d arr, int height, int width) {
+
+    for(int i = 0; i < height; i++) {
+
+        cout << "\n\tNodes: " << groups[i] << "\n\t\t"; 
+
+        for(int j = 0; j < width; j++) {
+
+            cout << setw(8) << arr.a[i][j] << "ms   ";
+        }
+        cout << "\n";
+    }
+}
