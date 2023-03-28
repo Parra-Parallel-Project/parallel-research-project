8d826d2295462cb11ea7483e0f09d5df6f1c4288
kazetoke <tiuna.saenz@gmail.com>
2023-03-28 08:38:43 -0400

    Added Dijkstra and included <queue>

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -40,7 +40,6 @@ void unthreadedDFS(int start,int stop, WeightedGraph& graph) {
         }
     }
 }
-
 //SingleDijkstra
 	//?some way to track nodes accessible and total distance/weights from source node?
 	//create priority queue PQ
@@ -48,6 +47,37 @@ void unthreadedDFS(int start,int stop, WeightedGraph& graph) {
 		//pick from the front of the PQ
 		//is this the target node?
 		//add neighbors to PQ if they don't have a lower cost path there
+void unthreadedDijkstra(int start, int stop, WeightedGraph& graph) {
+    const int INF = std::numeric_limits<int>::max();
+    vector<int> dist((graph.getnNodes())+1, INF);
+    std::priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push(make_pair(0, start));
+    dist[start] = 0;
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        if (u == stop){
+            break;
+        }
+
+        for (std::list<edge>::iterator it = graph.getEdges(u)->begin(); it != graph.getEdges(u)->end(); ++it) {
+            int v = it->first;
+            int weight = it->second;
+
+            if (dist[v] > dist[u] + weight) {
+                dist[v] = dist[u] + weight;
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+        if (dist[stop] == INF) {
+        //cout << "doesnt exist" << endl;
+    } else {
+        //cout << "from " << start << " to " << stop << "distance is: " << dist[stop] << endl;
+    }
+}
 
 
 //Multi Thread Section
@@ -117,4 +147,73 @@ void threadedDFS(int start, WeightedGraph& graph, int threadCount) {
         worker.join();
     }
 }
+
 //MultiDijkstra
+
+void subThreadDijkstra(WeightedGraph& graph, vector<int>& dist, vector<bool>& visited, int start, int stop) {
+    const int INF = std::numeric_limits<int>::max();
+
+    // Initialize all distance values to infinity
+    for (int i = 1; i <= graph.getnNodes(); i++) {
+        dist[i] = INF;
+    }
+    dist[start] = 0;
+
+    while (true) {
+        // Find smallest unvisited node
+        int u = -1;
+        int minDist = INF;
+        for (int i = 1; i <= graph.getnNodes(); i++) {
+            if (!visited[i] && dist[i] < minDist) {
+                u = i;
+                minDist = dist[i];
+            }
+        }
+
+        if (u == -1 || u == stop) {
+            break;
+        }
+
+        // Mark as visited
+        visited[u] = true;
+
+        // Update the distances
+        for (std::list<edge>::iterator it = graph.getEdges(u)->begin(); it != graph.getEdges(u)->end(); ++it) {
+            int v = it->first;
+            int weight = it->second;
+
+            if (!visited[v] && dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+            }
+        }
+    }
+}
+void threadedDijkstra(int start, int stop, WeightedGraph& graph, int numberThreads) {
+    const int INF = std::numeric_limits<int>::max();
+    vector<thread> threads;
+    vector<vector<int>> dist(numberThreads, vector<int>(graph.getnNodes()+1, INF));
+    vector<vector<bool>> visited(numberThreads, vector<bool>(graph.getnNodes()+1, false));
+
+    for (int t = 0; t < numberThreads; t++) {
+        int subgraphSize = graph.getnNodes() / numberThreads;
+        int subgraphStart = t * subgraphSize + 1;
+        int subgraphStop = (t == numberThreads-1) ? graph.getnNodes() : (t+1) * subgraphSize;
+
+        threads.emplace_back(subdijkstra, std::ref(graph), std::ref(dist[t]), std::ref(visited[t]), subgraphStart, stop);
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    int finalDist = INF;
+    for (int t = 0; t < numberThreads; t++) {
+        finalDist = min(finalDist, dist[t][stop]);
+    }
+
+    if (finalDist == INF) {
+       // cout << "doesnt exist" << endl;
+    } else {
+       // cout << "from " << start << " to " << stop << " distance is: " << finalDist << endl;
+    }
+}
